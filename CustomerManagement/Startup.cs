187b8592cbcaaec3fc91b457155d0250f8a03f78
Jack.Tseng <Jack.Tseng@N7IT375>
2016-10-29 01:12:59 +0800

    加入專案檔案。

---
+++ CustomerManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomerManagement.Startup))]
+namespace CustomerManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
